093233167de934ff464ae2664660fb1893981664
Coding4learning <87975613+Coding4learning@users.noreply.github.com>
2021-08-30 19:06:41 +0900

    Update 2-Using else if.swift

--- Code 1/4-Conditional Code/2-Using else if.swift
+++ Code 1/4-Conditional Code/2-Using else if.swift
@@ -1,1 +1,15 @@
 //
+moveForward()
+if isOnClosedSwitch {
+    toggleSwitch()
+}
+else if isOnGem{
+    collectGem()
+}
+moveForward()
+if isOnGem{
+    collectGem()
+}
+else if isOnClosedSwitch{
+    toggleSwitch()
+}
