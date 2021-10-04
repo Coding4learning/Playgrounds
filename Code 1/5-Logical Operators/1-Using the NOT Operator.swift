9e7d12698117d36373f3d6ecca4538ce916ae818
Coding4learning <87975613+Coding4learning@users.noreply.github.com>
2021-10-04 19:04:10 +0900

    Update 1-Using the NOT Operator.swift

--- Code 1/5-Logical Operators/1-Using the NOT Operator.swift
+++ Code 1/5-Logical Operators/1-Using the NOT Operator.swift
@@ -1,1 +1,27 @@
-//
+/* I need to make byte go down the stairs and come back up. after that, i need to make him collect the rest of the gems.
+ if !isOnGem{
+ go down stairs and come back up.
+ }else{
+ collect gem
+ {
+ */
+ for i in 1 ... 4 {
+ moveForward()
+ if !isOnGem {
+    turnLeft()
+    moveForward()
+    moveForward()
+    collectGem()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+    turnLeft()
+    
+ }
+ else{
+    collectGem()
+ }
+ }
+ 
+
