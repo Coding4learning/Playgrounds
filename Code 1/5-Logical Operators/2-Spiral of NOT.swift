b72653983a15f80f16c32c9bfac8eff9d3b34581
Coding4learning <87975613+Coding4learning@users.noreply.github.com>
2021-10-04 19:03:22 +0900

    Update 2-Spiral of NOT.swift

--- Code 1/5-Logical Operators/2-Spiral of NOT.swift
+++ Code 1/5-Logical Operators/2-Spiral of NOT.swift
@@ -1,1 +1,16 @@
-//
+/* i need to turn left if blocked.(if there is no block in front of me)
+ if isBlocked{
+ turnLeft
+ }
+ */
+for i in 1 ... 13 {
+    if !isBlocked{
+        moveForward()
+    }else{
+        turnLeft()
+        moveForward()
+    }
+    
+}
+toggleSwitch()
+
